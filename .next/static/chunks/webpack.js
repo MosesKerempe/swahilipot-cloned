8f0eebf155cd45c14277fb4be824b80a8bbb835c
ian_muriuki <ianmuriuki.inc@gmail.com>
2025-03-04 20:47:25 +0300

    minor changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e0491dbad3b6d40"; }
+/******/ 		__webpack_require__.h = function() { return "aef349441763b4c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
