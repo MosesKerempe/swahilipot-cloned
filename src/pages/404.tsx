1405897aa357c0b63f09427eea0ce3679287e5b6
ian_muriuki <ianmuriuki.inc@gmail.com>
2025-03-04 20:52:23 +0300

    update 404 file

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,8 +5,8 @@ export default function Custom404() {
     <div className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-primary mb-4">404</h1>
-        <p className="text-2xl mb-8">Page Not Found</p>
-        <p className="mb-8">The page you are looking for doesn't exist or has been moved.</p>
+        <p className="text-2xl mb-8">Ooops!! Page Not Found</p>
+        <p className="mb-8">The page you are looking for is still under development, Come back later😬😬😬.</p>
         <Link href="/" className="btn">
           Go back home
         </Link>
