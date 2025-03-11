73e3d13c8ab83e00a5bad211d0db55d1178c0427
ian_muriuki <ianmuriuki.inc@gmail.com>
2025-03-11 10:05:00 +0300

    deployment errors resolve attempts

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'json',
+      resolve: {
+        fullySpecified: false
+      }
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
--- src/layout/MetaTags.tsx
+++ src/layout/MetaTags.tsx
@@ -6,19 +6,10 @@ interface MetaTagsProps {
   image?: string;
 }
 
-/**
- * A component to manage the meta tags in the HTML document.
- * This includes the title, description, image, and other metadata that can be used by search engines and social media platforms.
- * The component takes in a MetaTagsProps object which can be used to customize the metadata.
- * @param {string} [title=SwahiliPot Hub - Empowering Innovation in Coastal Region] The title of the page.
- * @param {string} [description=SwahiliPot Hub is a collaborative space for technology, arts, and culture in Mombasa, Kenya.] The description of the page.
- * @param {string} [image=/images/logo.png] The URL of the image to be used as the thumbnail when sharing the page on social media platforms.
- * @returns {JSX.Element} The HTML element that contains the meta tags.
- */
 export default function MetaTags({
-  title = 'SwahiliPot Hub - Empowering Innovation in Coastal Regions',
+  title = 'SwahiliPot Hub - Empowering Innovation in East Africa',
   description = 'SwahiliPot Hub is a collaborative space for technology, arts, and culture in Mombasa, Kenya.',
-  image = '/images/logo.png',
+  image = '/images/og-image.jpg',
 }: MetaTagsProps) {
   return (
     <Head>
